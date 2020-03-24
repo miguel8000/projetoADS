e2c1abea2652294b268bd0af625db73ca4034c91
Victor Falcão <victorfalcao33@gmail.com>
2020-03-24 17:32:30 -0300

    Merge branch 'master' of https://github.com/VictorGameOn/projetoADS

--- Login_ADM.php
+++ Login_ADM.php
@@ -3,34 +3,25 @@ session_start();
 include("conexao.php");
 
 if (empty($_POST['usuario']) || empty($_POST['senha'])) {
-	header('Location: loginADM.html');
+	header('Location: telaLoginADM.html');
 	exit();
 }
 
 $login_admin = $_POST['usuario'];
 $senha_admin = $_POST['senha'];
-#$nickname_cliente = $_POST['usuario'];
-#$senha_cliente = $_POST['senha'];
 
 $query = "SELECT login_admin FROM admin WHERE login_admin = '{$login_admin}' AND senha_admin = md5('{$senha_admin}')";
-#$query2 = "SELECT nickname_cliente FROM cliente WHERE nickname_cliente = '{$nickname_cliente}' AND senha_cliente = md5('{$senha_cliente}')";
 
 $result = mysqli_query($conexao, $query);
 $row = mysqli_num_rows($result);
-#$result2 = mysqli_query($conexao, $query2);
-#$row2 = mysqli_num_rows($result2);
 
 if($row == 1){
 	$_SESSION['usuario'] = $login_admin;
-	header('Location: admArea.html');
+	header('Location: telaAdmRelatorio.html');
 	exit();
-}/*elseif($row2 == 1){
-	$_SESSION['usuario'] = $nickname_cliente;
-	header('Location: CadastroServiço.html');
-	exit();
-}*/else{
+}else{
 	$_SESSION['nao_autenticado'] = true;
-	header('Location: loginADM.html');
+	header('Location: telaLoginADM.html');
 	exit();
 }
 
